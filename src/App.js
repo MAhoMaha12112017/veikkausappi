d4ca35684890720c4fbd0f5df9b13c3f1fc8174b
Matti Aho <ookooko@gmail.com>
2018-10-28 13:41:56 +0200

    newly created containers seem to be working like before division

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Joukkueet from './components/Joukkueet/Joukkueet';
 import Navigation from './components/Navigation/Navigation';
-import Tulos from './components/Tulos/Tulos';
-import Liigat from './components/Liigat/Liigat';
-import HomeAway from './components/HomeAway/HomeAway';
 import Search from './components/Search/Search';
 import SaveMatch from './components/SaveMatch/SaveMatch';
 import ExtendedData from './components/ExtendedData/ExtendedData';
 import MatchResults from './components/MatchResults/MatchResults';
 import teamList from './components/Joukkueet/joukkuelista.js';
 import footballicon from './icons8-soccer-ball-48.png';
-import fetchteamdata from './helpers/fetchteamdata';
+// import Tulos from './components/Tulos/Tulos';
+// import Liigat from './components/Liigat/Liigat';
+// import HomeAway from './components/HomeAway/HomeAway';
+// import Joukkueet from './components/Joukkueet/Joukkueet';
+// import fetchteamdata from './helpers/fetchteamdata';
 // import savematchdata from './helpers/savematchdata';
-import fetchmatchsearch from './helpers/fetchmatchsearch';
-
+// import fetchmatchsearch from './helpers/fetchmatchsearch';
+// import logo from './logo.svg';
 
 const defaults = {
   id: 0,
@@ -43,148 +42,110 @@ class App extends Component {
   }
 
   // for edits of data
-  onChangehomeGoals = (event) => {
-    this.setState({homeGoals: Number(event.target.value)})
-  }
-  onChangeawayGoals = (event) => {
-    this.setState({awayGoals: Number(event.target.value)})
-  }
-  onChangehomexG = (event) => {
-    this.setState({homexG: Number(event.target.value)})
-  }
-  onChangeawayxG = (event) => {
-    this.setState({awayxG: Number(event.target.value)})
-  }
-  onChangeRound = (event) => {
-    this.setState({round: Number(event.target.value)})
-  }
-  onChangeId = (event) => {
-    this.setState({id: Number(event.target.value)})
-  }
-  onHomeTeamChange = (event) => { 
-    this.setState({homeTeam: event.target.value})
-  }
-  onAwayTeamChange = (event) => { 
-    this.setState({awayTeam: event.target.value})
-  }
-  onLeagueChange = (event) => {
-    this.setState({league: event.target.value});
-  }
-  onHomeAwayChange = (event) => {
-    this.setState({homeaway: event.target.value});
-  }
-  onSelectedTeamChange = (event) => {
-    this.setState({selectedTeam: event.target.value});
-  }
+  // onChangehomeGoals = (event) => {
+  //   this.setState({homeGoals: Number(event.target.value)})
+  // }
+  // onChangeawayGoals = (event) => {
+  //   this.setState({awayGoals: Number(event.target.value)})
+  // }
+  // onChangehomexG = (event) => {
+  //   this.setState({homexG: Number(event.target.value)})
+  // }
+  // onChangeawayxG = (event) => {
+  //   this.setState({awayxG: Number(event.target.value)})
+  // }
+  // onChangeRound = (event) => {
+  //   this.setState({round: Number(event.target.value)})
+  // }
+  // onChangeId = (event) => {
+  //   this.setState({id: Number(event.target.value)})
+  // }
+  // onHomeTeamChange = (event) => { 
+  //   this.setState({homeTeam: event.target.value})
+  // }
+  // onAwayTeamChange = (event) => { 
+  //   this.setState({awayTeam: event.target.value})
+  // }
+  // onLeagueChange = (event) => {
+  //   this.setState({league: event.target.value});
+  // }
+  // onHomeAwayChange = (event) => {
+  //   this.setState({homeaway: event.target.value});
+  // }
+  // onSelectedTeamChange = (event) => {
+  //   this.setState({selectedTeam: event.target.value});
+  // }
  
-  // ohjelman tilan muutos, tuottaa erilaisia tulostusvaihtoehtoja. Vähällä käytöllä, poistettava tai lisättävä vaihtoehtoja
+  // used by Navigation
   onUseChange = (use) => {
-    if (use ==='search') {  
-      this.setState({
-        use,
-        homeGoals: 0, // voisko laittaa null / undefined?
-        awayGoals: 0,
-        homexG: 0,
-        awayxG: 0,
-        selectedTeam: ''
-      });
-    } else if (use === 'save') { 
-      this.setState({use}) 
-    }
+    // if (use ==='search') {  
+      this.setState({use});
+        // homeGoals: 0, // voisko laittaa null / undefined?
+        // awayGoals: 0,
+        // homexG: 0,
+        // awayxG: 0,
+        // selectedTeam: ''
+    // } else if (use === 'save') { 
+    //   this.setState({use}) 
+    // }
   }
 
-  // onButtonSave = () => {
-  //   savematchdata('http://localhost:3001/match', this.state)
+  // // datan haku - perusdata
+  // onButtonSearch = () => {
+  //   const searchBody = {
+  //     id: this.state.id || undefined,
+  //     league: this.state.league || undefined,
+  //     round: this.state.round || undefined,
+  //     team1: this.state.homeTeam || undefined,
+  //     team2: this.state.awayTeam || undefined,
+  //     homeaway: this.state.homeaway || undefined,
+  //     team: this.state.selectedTeam || undefined
+  //   }
+  //   console.log('onButtonSearch.searchBody', searchBody);
+
+  //   fetchmatchsearch('http://localhost:3001/matchsearch', searchBody)
   //   .then((data) => {
-  //     console.log('savematchdata, returned data ', data);
-  //     if (data.id) {
-  //       alert(`Match between ${data.hometeamabbr} and ${data.awayteamabbr} added to database`)
+  //     if (data[0].id) {
+  //       console.log(data);
+  //       this.setState({use: 'matchresults', results: data}); // siirto results-näytölle
   //     } else {
-  //       alert(`Match could not be added to database`);
+  //       alert('data not found')
   //     }
-  //   });
+  //   })
+  //   .catch((err) => console.log(err));
   // }
 
-  // datan haku - perusdata
-  onButtonSearch = () => {
-    const searchBody = {
-      id: this.state.id || undefined,
-      league: this.state.league || undefined,
-      round: this.state.round || undefined,
-      team1: this.state.homeTeam || undefined,
-      team2: this.state.awayTeam || undefined,
-      homeaway: this.state.homeaway || undefined,
-      team: this.state.selectedTeam || undefined
-    }
-    console.log('onButtonSearch.searchBody', searchBody);
-
-    fetchmatchsearch('http://localhost:3001/matchsearch', searchBody)
-    .then((data) => {
-      if (data[0].id) {
-        console.log(data);
-        this.setState({use: 'matchresults', results: data}); // siirto results-näytölle
-      } else {
-        alert('data not found')
-      }
-    })
-    .catch((err) => console.log(err));
-  }
-
   // datan haku - laajennettu data
-  onButtonExtendedSingleTeamData = () => {
-    const searchBody = {
-      team: this.state.selectedTeam || undefined,
-      homeaway: this.state.homeaway || undefined
-    }
+  // onButtonExtendedSingleTeamData = () => {
+  //   const searchBody = {
+  //     team: this.state.selectedTeam || undefined,
+  //     homeaway: this.state.homeaway || undefined
+  //   }
    
-    if (this.state.homeaway === 'home' || this.state.homeaway === 'away') {
-      console.log('searchBody.homeaway ', searchBody.homeaway)
-      fetchteamdata('http://localhost:3001/teamdata', searchBody)
-      .then(response => {
-        console.log('home or away', response);
-        this.setState({use: 'extendedData', extendedResults: response}); // toteutus puuttuu?
-      });
-    } else if (this.state.homeaway === 'all') {  // todo, oltava yhdistetty data, mergeHomeAndAwayData?
-        console.log('haetaan molemmat ja yhdistetään data');
-        searchBody.homeaway = 'home';
-        fetchteamdata('http://localhost:3001/teamdata', searchBody)
-        .then(response => {
-          this.setState({use: 'extendedData', homeResults: response});
-        });
-        searchBody.homeaway = 'away';
-        fetchteamdata('http://localhost:3001/teamdata', searchBody)
-        .then(response => {
-          this.setState({use: 'extendedData', awayResults: response});
-        });
-    }
-  }
-
-  mergeHomeAndAwayData = (homeData, awayData) => {
-    const goals = homeData.HomeGoals + awayData.AwayGoals;
-    const xGoals = homeData.HomeXG + awayData.AwayXG; 
-    const wins = homeData.wins + awayData.wins;
-    const draws = homeData.draws + awayData.draws;
-    const losses = homeData.losses + awayData.losses;
-    const games = homeData.lenght + awayData.length;
-    const avgGoals = goals / games;
-    const avgXG = xGoals / games;
-    return {
-      goals,
-      xGoals,
-      wins,
-      draws,
-      losses,
-      games,
-      avgGoals,
-      avgXG
-    } 
-  }
+  //   if (this.state.homeaway === 'home' || this.state.homeaway === 'away') {
+  //     console.log('searchBody.homeaway ', searchBody.homeaway)
+  //     fetchteamdata('http://localhost:3001/teamdata', searchBody)
+  //     .then(response => {
+  //       console.log('home or away', response);
+  //       this.setState({use: 'extendedData', extendedResults: response}); // toteutus puuttuu?
+  //     });
+  //   } else if (this.state.homeaway === 'all') {  // todo, oltava yhdistetty data, mergeHomeAndAwayData?
+  //       console.log('haetaan molemmat ja yhdistetään data');
+  //       searchBody.homeaway = 'home';
+  //       fetchteamdata('http://localhost:3001/teamdata', searchBody)
+  //       .then(response => {
+  //         this.setState({use: 'extendedData', homeResults: response});
+  //       });
+  //       searchBody.homeaway = 'away';
+  //       fetchteamdata('http://localhost:3001/teamdata', searchBody)
+  //       .then(response => {
+  //         this.setState({use: 'extendedData', awayResults: response});
+  //       });
+  //   }
+  // }
 
   onClickReset = () => {
     this.setState(defaults);
-  }
-  onClickSearchReset = () => {
-    this.setState({...defaults, use: 'search'});
   }
   onClickTable = () => {
     console.log('onClickTable clicked');
@@ -197,37 +158,15 @@ class App extends Component {
 
     // different data showed based to state.use
     let show = '';
-    
       if (this.state.use === 'matchresults') { 
         show =  <MatchResults results={this.state.results} />
       } else if (this.state.use === 'extendedData') { 
         show = 
           <ExtendedData extendedResults={this.state.extendedResults} selectedTeam={this.state.selectedTeam} homeaway={this.state.homeaway} />
       } else if (this.state.use === 'search' ) { // hakuikkuna
-        show = (
-          <div>
-            <Search />
-            <Liigat onLeagueChange={this.onLeagueChange} labeli="League" currentValue={this.state.league}/>
-            <Tulos onResultChange={this.onChangeRound} labeli="Round" currentValue={this.state.round} />
-            <p>Team Pair Data:</p>
-            <Joukkueet onTeamChange={this.onHomeTeamChange} labeli="Team 1" league={this.state.league} currentValue={this.state.homeTeam}/>
-            <Joukkueet onTeamChange={this.onAwayTeamChange} labeli="Team 2" league={this.state.league} currentValue={this.state.awayTeam}/>
-            <button onClick={this.onButtonExtendedTeamPairData}>Extended Team Pair Data</button>
-            <hr />
-            <Joukkueet onTeamChange={this.onSelectedTeamChange} labeli="Single Team Match Data" league={this.state.league} currentValue={this.state.selectedTeam}/>
-            <HomeAway onHomeAwayChange={this.onHomeAwayChange} labeli="Home / Away" currentValue={this.state.homeaway} />
-            <button onClick={this.onButtonExtendedSingleTeamData}>Extended Single Team Data</button>
-            <hr />
-            <Tulos onResultChange={this.onChangeId} labeli="Id" currentValue={this.state.id} />
-            <button onClick={this.onButtonSearch}>Hae otteludata</button>
-            <button onClick={this.onClickSearchReset} type="button">Tyhjennä</button>
-          </div>
-        )
+        show = <Search />
       } else if (this.state.use === 'save') {  // syöttöikkuna 
-        show =  (
-        <SaveMatch />
-        
-        ) 
+        show = <SaveMatch />
       } 
 
       return (
@@ -257,4 +196,31 @@ export default App;
           <button onClick={this.onButtonSave}>Lähetä</button>
           <button onClick={this.onClickReset} type="button">Tyhjennä</button>
         </div>
+        --- search:
+        <Liigat onLeagueChange={this.onLeagueChange} labeli="League" currentValue={this.state.league}/>
+            <Tulos onResultChange={this.onChangeRound} labeli="Round" currentValue={this.state.round} />
+            <p>Team Pair Data:</p>
+            <Joukkueet onTeamChange={this.onHomeTeamChange} labeli="Team 1" league={this.state.league} currentValue={this.state.homeTeam}/>
+            <Joukkueet onTeamChange={this.onAwayTeamChange} labeli="Team 2" league={this.state.league} currentValue={this.state.awayTeam}/>
+            <button onClick={this.onButtonExtendedTeamPairData}>Extended Team Pair Data</button>
+            <hr />
+            <Joukkueet onTeamChange={this.onSelectedTeamChange} labeli="Single Team Match Data" league={this.state.league} currentValue={this.state.selectedTeam}/>
+            <HomeAway onHomeAwayChange={this.onHomeAwayChange} labeli="Home / Away" currentValue={this.state.homeaway} />
+            <button onClick={this.onButtonExtendedSingleTeamData}>Extended Single Team Data</button>
+            <hr />
+            <Tulos onResultChange={this.onChangeId} labeli="Id" currentValue={this.state.id} />
+            <button onClick={this.onButtonSearch}>Hae otteludata</button>
+            <button onClick={this.onClickSearchReset} type="button">Tyhjennä</button>
 */}
+
+  // onButtonSave = () => {
+  //   savematchdata('http://localhost:3001/match', this.state)
+  //   .then((data) => {
+  //     console.log('savematchdata, returned data ', data);
+  //     if (data.id) {
+  //       alert(`Match between ${data.hometeamabbr} and ${data.awayteamabbr} added to database`)
+  //     } else {
+  //       alert(`Match could not be added to database`);
+  //     }
+  //   });
+  // }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-// import Joukkueet from '../../components/Joukkueet/Joukkueet';
-// import Tulos from '../../components/Tulos/Tulos';
-// import Liigat from '../../components/Liigat/Liigat';
-// import HomeAway from '../../components/HomeAway/HomeAway';
+import Joukkueet from '../../components/Joukkueet/Joukkueet';
+import Tulos from '../../components/Tulos/Tulos';
+import Liigat from '../../components/Liigat/Liigat';
+import HomeAway from '../../components/HomeAway/HomeAway';
 import teamList from '../../components/Joukkueet/joukkuelista.js';
+import fetchmatchsearch from '../../helpers/fetchmatchsearch';
+import fetchteamdata from '../../helpers/fetchteamdata';
 
 const defaults = {
   id: 0,
@@ -11,10 +13,10 @@ const defaults = {
   round: 0,
   homeTeam: '',
   awayTeam: '',
-  homeGoals: 0,
-  awayGoals: 0,
-  homexG: 0,
-  awayxG: 0
+  homeaway: 'all',
+  selectedTeam: '',
+  results: {}, // miten käytetään?
+  extendedResults: {} // miten käytetään?
 }
 
 class Search extends Component {
@@ -22,10 +24,116 @@ class Search extends Component {
     super(props);
     this.state = defaults;
   }
+
+  // button events handlers
+  onButtonSearch = () => {
+    const searchBody = {
+      id: this.state.id || undefined,
+      league: this.state.league || undefined,
+      round: this.state.round || undefined,
+      team1: this.state.homeTeam || undefined,
+      team2: this.state.awayTeam || undefined,
+      homeaway: this.state.homeaway || undefined,
+      team: this.state.selectedTeam || undefined
+    }
+    console.log('onButtonSearch.searchBody', searchBody);
+
+    fetchmatchsearch('http://localhost:3001/matchsearch', searchBody)
+    .then((data) => {
+      if (data[0].id) {
+        console.log(data);
+        this.setState({results: data}); // use: 'matchresults', siirto results-näytölle 
+      } else {
+        alert('data not found')
+      }
+    })
+    .catch((err) => console.log(err));
+  }
+
+  onButtonExtendedSingleTeamData = () => {
+    // search parameters to object
+    const searchBody = {
+      team: this.state.selectedTeam || undefined,
+      homeaway: this.state.homeaway || undefined
+    }
+   
+    if (this.state.homeaway === 'home' || this.state.homeaway === 'away') {
+      console.log('searchBody.homeaway ', searchBody.homeaway)
+      fetchteamdata('http://localhost:3001/teamdata', searchBody)
+      .then(response => {
+        console.log('home or away', response);
+        this.setState({extendedResults: response}); // use: 'extendedData', toteutus puuttuu? 
+      });
+    } else if (this.state.homeaway === 'all') {  // todo, oltava yhdistetty data, mergeHomeAndAwayData?
+        console.log('haetaan molemmat ja yhdistetään data');
+        searchBody.homeaway = 'home';
+        fetchteamdata('http://localhost:3001/teamdata', searchBody)
+        .then(response => {
+          this.setState({homeResults: response}); // use: 'extendedData', 
+        });
+        searchBody.homeaway = 'away';
+        fetchteamdata('http://localhost:3001/teamdata', searchBody)
+        .then(response => {
+          this.setState({awayResults: response}); // use: 'extendedData', 
+        });
+    }
+  }
+
+  mergeHomeAndAwayData = (homeData, awayData) => { // vois siirtää jonnekin muualle tästä tilaa viemästä!? helpersseihin?
+    const goals = homeData.HomeGoals + awayData.AwayGoals;
+    const xGoals = homeData.HomeXG + awayData.AwayXG; 
+    const wins = homeData.wins + awayData.wins;
+    const draws = homeData.draws + awayData.draws;
+    const losses = homeData.losses + awayData.losses;
+    const games = homeData.lenght + awayData.length;
+    const avgGoals = goals / games;
+    const avgXG = xGoals / games;
+    return {
+      goals,
+      xGoals,
+      wins,
+      draws,
+      losses,
+      games,
+      avgGoals,
+      avgXG
+    } 
+  }
+
+  onButtonExtendedTeamPairData = () => {
+    console.log('onButtonExtendedTeamPairData')
+  }
+
+  onClickSearchReset = () => {
+    this.setState({...defaults}); // use: 'search'
+  }
+
+  // form field edits handeled
+  onChangeRound = (event) => {
+    this.setState({round: Number(event.target.value)})
+  }
+  onChangeId = (event) => {
+    this.setState({id: Number(event.target.value)})
+  }
+  onHomeTeamChange = (event) => { 
+    this.setState({homeTeam: event.target.value})
+  }
+  onAwayTeamChange = (event) => { 
+    this.setState({awayTeam: event.target.value})
+  }
+  onLeagueChange = (event) => {
+    this.setState({league: event.target.value});
+  }
+  onHomeAwayChange = (event) => {
+    this.setState({homeaway: event.target.value});
+  }
+  onSelectedTeamChange = (event) => {
+    this.setState({selectedTeam: event.target.value});
+  }
+
   render() {
     return(
       <div>
-      {/*
         <Liigat onLeagueChange={this.onLeagueChange} labeli="League" currentValue={this.state.league}/>
         <Tulos onResultChange={this.onChangeRound} labeli="Round" currentValue={this.state.round} />
         <p>Team Pair Data:</p>
@@ -40,7 +148,6 @@ class Search extends Component {
         <Tulos onResultChange={this.onChangeId} labeli="Id" currentValue={this.state.id} />
         <button onClick={this.onButtonSearch}>Hae otteludata</button>
         <button onClick={this.onClickSearchReset} type="button">Tyhjennä</button>
-      */}
       </div>
     )
   }
